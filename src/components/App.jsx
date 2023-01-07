3a3e11ed66e8527c3cf25174cf4ad7465ba5bbad
mehmetali10 <mhmtali2316@gmail.com>
2023-01-08 02:23:36 +0300

    add "add & remove note" functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+import note from "../notes.js";
 
-function App() {
+export default function App() {
+  const [notes, setNotes] = useState(note);
+
+  function addItem(newNote){
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  
   return (
     <div>
       <Header />
-      {notes.map( n => <Note key={n.key} title={n.title} content={n.content} />)}
+      <CreateArea onAdd={addItem}/>
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer name="Mehmet Ali Mergen"/>
     </div>
   );
 }
 
-export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-
-class Note extends React.Component{
-    render(){
-        return(<div className="note">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.content}</p>
-        </div>);
-    }
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={()=>{
+        props.onDelete(props.id);
+      }}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
